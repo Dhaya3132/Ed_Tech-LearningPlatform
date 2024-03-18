4a048191f98a3c9a3021d729ac1b5900260db85a
Dhaya3132 <dhayalanu103@gmail.com>
2024-03-18 22:18:40 +0530

    fat:making navbar responsive

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+const bar_icon = document.querySelector('.bar-icon');
+const sidebar = document.querySelector('.sidebar');
+
+
+bar_icon.addEventListener('click',(e)=>
+{
+   sidebar.style.display='flex';
+});
